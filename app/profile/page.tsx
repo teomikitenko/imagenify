9d02860355e49f29777b232a494018b41a032d31
Yevhenii <jenmikitenko@gmail.com>
2024-03-17 13:06:08 +0200

    fix deployment bugs 2

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,7 +7,6 @@ const Profile = async () => {
     <div className="w-full pt-11 lg:pt-0 flex flex-col gap-3 lg:gap-10">
       <h1 className="responsive-text font-extrabold text-blue-950 mb-3 dark:text-slate-200">Profile</h1>
       <Suspense fallback={<LoadingProfile/>}>
-        {/* @ts-expect-error Server Component */}
         <ProfileWrapperImage />
       </Suspense>
     </div>
