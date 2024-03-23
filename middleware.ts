01bdff00ffc1bb8167341252507b6879725c09f0
Yevhenii <jenmikitenko@gmail.com>
2024-03-23 14:05:47 +0200

    test middleware

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { authMiddleware } from "@clerk/nextjs";
 
   export default authMiddleware({
     publicRoutes: ['/',"/api/webhooks(.*)"],
+    beforeAuth:(req)=>console.log(req),
+    afterAuth:(req)=>console.log(req)
 });  
 
 export const config = {
