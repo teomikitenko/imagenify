0aa1ab1edb3f07417cad30f5c122214d87088e68
Yevhenii <jenmikitenko@gmail.com>
2024-03-24 18:42:23 +0200

    final fix 2.2

--- components/BurgerMenu.tsx
+++ components/BurgerMenu.tsx
@@ -50,7 +50,7 @@ const BurgerMenu = ({
     return () => clearTimeout(timer);
   }, [close]);
   return (
-    <div className="h-screen z-20 fixed top-0 w-full flex">
+    <div className="h-full z-20 fixed top-0 w-full flex">
       <div className="overflow-scroll relative w-full h-full">
         <div
           onClick={() => setClose(true)}
@@ -66,7 +66,7 @@ const BurgerMenu = ({
             >
               &#10005;
             </span>
-            <div className="flex flex-col h-full pt-3">
+            <div className="flex flex-col h-[calc(100vh-25%)] pt-3">
               <Image src={Logo} width={153} alt="logo" />
               <div className="flex flex-col h-full justify-between ">
                 <div className="mt-8 flex flex-col  gap-5">
